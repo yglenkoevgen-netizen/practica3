5572f20dd6cf346801a737206b776e130fb6c617
Угленко Євгеній <yglenko.evgen@e-u.edu.ua>
2025-11-09 14:53:57 +0200

    Finalizing react component and deploy setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import BlogCard from './components/BlogCard';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const cardData = {
+    image: "/assets/Blog Image.png",
+    category: "HTML & CSS",
+    date: "Публікація 21 Вересня 2025",
+    description: (
+      <>
+        Мета практичної роботи полягає в тому, щоб налаштувати середовище розробки 
+        <strong> VS Code</strong>, а також реалізувати картку блогу для вдосконалення 
+        фундаментальних навичок з <strong>HTML та CSS</strong>.
+      </>
+    ),
+    title: "Практична №3",
+    authorImage: "/assets/avatar_male.png",
+    authorName: "Угленко Євгеній"
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <BlogCard 
+        {...cardData}
+      />
+    </main>
+  );
 }
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+const repoName = "practica3"; 
 export default defineConfig({
   plugins: [react()],
-})
+  base: `/${repoName}/`, 
+});
